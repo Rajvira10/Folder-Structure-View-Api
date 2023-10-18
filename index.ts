489dda35bfac238ec9a52f052b41e84c53440fa7
Rajvir Ahmed Shuvo <rajvirahmedshuvo10@gmail.com>
2023-10-19 04:32:56 +0600

    set type to module

--- index.ts
+++ index.ts
@@ -36,6 +36,7 @@ app.use((req, res) => {
   res.status(404).json({ error: "Not Found" });
 });
 
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
